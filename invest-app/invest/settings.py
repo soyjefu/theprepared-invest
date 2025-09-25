03b19caed92340e934dd80797a775ce059e774cb
soyjefu <5ll34090@gmail.com>
2025-09-25 11:56:43 +0900

    database settings

--- invest-app/invest/settings.py
+++ invest-app/invest/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB_I'),
+        'NAME': os.environ.get('POSTGRES_DB'), # <--- 수정
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'invest_db',
--- invest-app/wait-for-postgres.sh
+++ invest-app/wait-for-postgres.sh
@@ -10,7 +10,7 @@ cmd="$@"
 
 # PGPASSWORD 환경 변수를 사용하여 psql 명령어 실행
 # -c '\q' 옵션은 간단한 연결 테스트 후 바로 종료하는 명령어입니다.
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB_I" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do # <--- 수정
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
