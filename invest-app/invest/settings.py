51b07ba67a1ec133730838822a379bbc3feec26d
soyjefu <5ll34090@gmail.com>
2025-09-26 16:17:26 +0900

    re set db-migration and mdify apps time zone

--- invest-app/invest/settings.py
+++ invest-app/invest/settings.py
@@ -106,8 +106,8 @@ CACHES = {
 }
 
 # --- Celery ---
-CELERY_BROKER_URL = 'redis://redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_BROKER_URL = 'redis://redis_invest:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis_invest:6379/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
